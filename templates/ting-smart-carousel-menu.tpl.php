6cfe52ed55f5c568b889a21ba858abfd76b86d6a
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-08-25 14:09:55 +0200

    Styling off menu and breadcrumb

---
+++ templates/ting-smart-carousel-menu.tpl.php
@@ -0,0 +1,8 @@
+<?php ?>
+
+<div class="ting-object-tree-menu">
+    <h2 class="sub-menu-title">
+        <a href="/bog/alle/alle/alle" class="active-trail">Kategorier</a>
+    </h2>
+    <?php print $menu; ?> 
+</div>
